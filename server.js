45ec2d8b428df6410f7839d96f3d86662f0a014a
Vinicius Bassalobre <=>
2021-03-22 19:06:26 -0300

    jwt

--- server.js
+++ server.js
@@ -1,14 +1,43 @@
 require('dotenv').config()
+const jwt = require("jsonwebtoken")
 const express = require('express')
 const app = express()
 app.use(express.json())
 
-app.get('/', (req, res) => {
-	res.send(process.env.APP_NAME)
+const verifyJWT = (req, res, next) => {
+	const token = req.headers["authorization"]
+	if (!token) res.sendStatus(403)
+	const splited = token.split(' ')
+	if (splited[0].trim().toLowerCase() != "bearer") res.sendStatus(403)
+	if (!splited[1]) res.sendStatus(403)
+	try {
+		jwt.verify(splited[1], process.env.SECRET_KEY, (er, decoded) => {
+			if (er) res.sendStatus(403)
+			req.user = decoded
+			next()
+		})
+	} catch (er) {
+		console.log("bbbb")
+		console.log(er)
+		res.sendStatus(403)
+	}
+}
+
+app.post('/', verifyJWT, (req, res) => {
+	res.status(202).json(req.user)
 })
 
-app.post('/test-post', (req, res) => {
-	res.send(req.body)
+app.post('/auth', (req, res) => {
+	//valida usuarios 
+
+
+	//cria token
+	let user = {
+		key: req.body.key,
+		secret: req.body.secret
+	}
+	let token = jwt.sign(user, process.env.SECRET_KEY, { expiresIn: '1h' })
+	res.status(202).json({ token })
 })
 
 
