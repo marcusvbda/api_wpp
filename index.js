bd41838088971b6a0f5390687e65440b07196787
Vinicius Bassalobre <=>
2021-03-23 18:39:49 -0300

    first commit

--- index.js
+++ index.js
@@ -8,14 +8,7 @@ app.get('/', (req, res) => {
 	res.send("api ...")
 })
 
-app.post('/webhook-simulate', (req, res) => {
-	let { event, data } = req.body
-	console.log("webhook-simulate", event, data)
-	return res.sendStatus(202)
-})
-
 app.use('/auth', require("@routes/auth"))
-app.use('/wpp', require("@routes/wpp"))
 
 const server_port = process.env.PORT || 3000
 app.listen(server_port, () => {
