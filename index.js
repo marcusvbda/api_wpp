2256ac730d0b5afa205ade7f0bd971b66ab7baaa
Vinicius Bassalobre <=>
2021-03-23 17:40:02 -0300

    vercel deploy

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.post('/webhook-simulate', (req, res) => {
 app.use('/auth', require("@routes/auth"))
 app.use('/wpp', require("@routes/wpp"))
 
-const server_port = process.env.PORT
+const server_port = process.env.PORT || 3000
 app.listen(server_port, () => {
 	console.log(`Server running in http://localhost:${server_port}`)
 })
